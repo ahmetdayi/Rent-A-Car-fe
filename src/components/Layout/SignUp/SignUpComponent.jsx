eac3a5b82a0caab73b8d30586811fa72c9744da4
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-03-10 15:47:41 +0300

    signup Page complated

--- src/components/Layout/SignUp/SignUpComponent.jsx
+++ src/components/Layout/SignUp/SignUpComponent.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import {ErrorMessage, Field, Form, Formik} from "formik";
+import React, {useState} from 'react';
+import {Form, Formik} from "formik";
 import validationSchema from "../../Services/SignUpValid";
+import Http from "../../Utility/Http";
+import Endpoints from "../../Utility/Endpoints";
 import classes from "./SignUpComponent.module.css"
+import {Navigate, useNavigate} from "react-router-dom";
 
 const SignUpComponent = () => {
 
+    let navigateFunction = useNavigate();
+    const [redirect, setRedirect] = useState(false);
 
 
     const onSubmit = () =>
-        (values, {setSubmitting}) => {
-            setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-            }, 400);
+        async (values, {setSubmitting}) => {
+            console.log(values)
+            const data = {
+                firstName: values.firstName,
+                lastName: values.lastName,
+                email: values.email,
+                password: values.password,
+                matchingPassword:values.matchingPassword
+            }
+
+            try{
+                const response= await Http.POST(Endpoints.CREATE_CUSTOMER, data, {})
+               navigateFunction("/login");
+            }catch (err){
+                console.log(err);
+            }
+
+            setSubmitting(false);
+            setRedirect(true);
         }
     ;
 
@@ -21,52 +40,90 @@ const SignUpComponent = () => {
             initialValues={{firstName: '', lastName: '', email: '', password: "", matchingPassword: ""}}
             validationSchema={validationSchema}
             onSubmit={onSubmit()}
+            enableReinitialize={true}
         >
-            {({errors, touched}) => (
-                <Form className={classes.form}>
+            {({
+                  values,
+                  errors,
+                  isSubmitting,
+                  touched,
+                  handleSubmit,
+                  handleChange
+              }
+            ) => (
+                <Form onSubmit={handleSubmit} className={classes.form}>
 
                     <label className={classes.label} htmlFor="firstName">First Name</label>
-
-                    <Field className={!touched.firstName
-                    || !errors.firstName ? `${classes.input}` : `${classes.error_input}`} id="firstName" name="firstName"
-                           type="text"/>
-
+                    <input
+                        className={!touched.firstName
+                        || !errors.firstName ? `${classes.input}` : `${classes.error_input}`} id="firstName"
+                        onChange={handleChange}
+                        name="firstName"
+                        type="text"
+                        value={values.firstName}
+                    />
                     {touched.firstName && errors.firstName &&
-                        <p className={classes.error_message}>{errors.firstName}</p>}
+                        <p className={classes.error_message}>{errors.firstName}</p>
+                    }
 
                     <label className={classes.label} htmlFor="lastName">Last Name</label>
-
-                    <Field className={!touched.lastName
-                    || !errors.lastName ? `${classes.input}` : `${classes.error_input}`} id="lastName" name="lastName"
-                           type="text"/>
-
-                    {touched.lastName && errors.lastName && <p className={classes.error_message}>{errors.lastName}</p>}
+                    <input
+                        className={!touched.lastName
+                        || !errors.lastName ? `${classes.input}` : `${classes.error_input}`} id="lastName"
+                        name="lastName"
+                        onChange={handleChange}
+                        value={values.lastName}
+                        type="text"
+                    />
+                    {
+                        touched.lastName && errors.lastName &&
+                        <p className={classes.error_message}>{errors.lastName}</p>
+                    }
 
                     <label className={classes.label} htmlFor="email">Email Address</label>
-
-                    <Field className={!touched.email
-                    || !errors.email ? `${classes.input}` : `${classes.error_input}`} id="email" name="email"
-                           type="email"/>
-
-                    {touched.email && errors.email && <p className={classes.error_message}>{errors.email}</p>}
+                    <input
+                        className={!touched.email
+                        || !errors.email ? `${classes.input}` : `${classes.error_input}`} id="email" name="email"
+                        onChange={handleChange}
+                        value={values.email}
+                        type="email"
+                    />
+                    {
+                        touched.email && errors.email && <p className={classes.error_message}>{errors.email}</p>
+                    }
 
                     <label className={classes.label} htmlFor="password">Password</label>
-
-                    <Field className={!touched.password
-                    || !errors.password ? `${classes.input}` : `${classes.error_input}`} id="password" name="password"
-                           type="password"/>
-
-                    {touched.password && errors.password && <p className={classes.error_message}>{errors.password}</p>}
+                    <input
+                        className={!touched.password
+                        || !errors.password ? `${classes.input}` : `${classes.error_input}`} id="password"
+                        name="password"
+                        onChange={handleChange}
+                        value={values.password}
+                        type="password"
+                    />
+                    {
+                        touched.password && errors.password &&
+                        <p className={classes.error_message}>{errors.password}</p>
+                    }
 
                     <label className={classes.label} htmlFor="matchingPassword">Matching Password</label>
-                    <Field
+                    <input
                         className={!touched.matchingPassword
                         || !errors.matchingPassword ? `${classes.input}` : `${classes.error_input}`}
-                        id="matchingPassword" name="matchingPassword" type="matchingPassword"/>
-                    {touched.matchingPassword && errors.matchingPassword &&
-                        <p className={classes.error_message}>{errors.matchingPassword}</p>}
+                        id="matchingPassword"
+                        onChange={handleChange}
+                        name="matchingPassword"
+                        value={values.matchingPassword}
+                        type="password"
+                    />"
+                    {
+                        touched.matchingPassword && errors.matchingPassword &&
+                        <p className={classes.error_message}>{errors.matchingPassword}</p>
+                    }
 
                     <button className={classes.button} type="submit">Submit</button>
+
+
                 </Form>
             )}
         </Formik>
